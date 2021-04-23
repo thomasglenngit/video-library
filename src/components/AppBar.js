284187f801fccbac88411d894916082419986d36
Thomas Glenn <thomasgrantglenn@mac.com>
2021-04-22 23:25:08 -0600

    add images and PeopleIcon for AppBar.

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import People from './PeopleIcon'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,6 +27,9 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <People />
+          </IconButton>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
---
+++ src/components/PeopleIcon.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const People = () => {
+  return (
+    <img src={'../images/people.png'} alt="a group of business executives"></img>
+  )
+}
+
+export default People
