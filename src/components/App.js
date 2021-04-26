e0ba98755e16b77c41df6fec428a937b270c098d
Thomas Glenn <thomasgrantglenn@mac.com>
2021-04-26 00:25:17 -0600

    remove superfluous import statements.

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Router, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
 // import SearchBar from './SearchBar'
 // import VideoList from './VideoList'
 // import VideoDetail from './VideoDetail'
